18623285ce4ebb27cc5a13e3f7c9904d9557e196
Ruslan Forostianov <ruslan@forostianov.com>
2023-07-07 09:31:29 +0200

    Expose h2 database for connection via network

--- src/main/java/nl/rabobank/jpa/JpaApplication.java
+++ src/main/java/nl/rabobank/jpa/JpaApplication.java
@@ -1,7 +1,11 @@
 package nl.rabobank.jpa;
 
+import org.h2.tools.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.sql.SQLException;
 
 @SpringBootApplication
 public class JpaApplication {
@@ -10,4 +14,8 @@ public class JpaApplication {
 		SpringApplication.run(JpaApplication.class, args);
 	}
 
+	@Bean(initMethod = "start", destroyMethod = "stop")
+	public Server h2Server() throws SQLException {
+		return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9092");
+	}
 }
